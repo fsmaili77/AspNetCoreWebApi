9f6b52041ff8c30da10b4451d5c1d7aca28f6614
Fehmir <fehmir@gmail.com>
2023-09-24 16:36:39 +0200

    Install packages; Add: models, DbContext, ConnectionStrings; Create Migration

---
+++ DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using CitiesManagerWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManagerWebAPI.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // DbContext  contsructor
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        // Create sample seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().HasData(new City()
+            {
+                //Convert string to Guid
+                CityID = Guid.Parse("04206C4E-B0A7-48F2-9159-27BDDDDC748C"), 
+                CityName = "Paris"
+            });
+            modelBuilder.Entity<City>().HasData(new City()
+            {
+                CityID = Guid.Parse("7EEFEBE3-5A43-4A89-818E-1C8AE50EC847"),
+                CityName = "London"
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
+using CitiesManagerWebAPI.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Defaults"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Forces browsers to enable Http protocol
+app.UseHsts();
 
 app.UseHttpsRedirection();
 
